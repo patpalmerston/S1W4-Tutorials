b80a42df0171bb799a4189937adc3c25eafd065e
patpalmerston <patpalmerston@gmail.com>
2019-06-18 16:08:50 -0700

    event updates

--- S2W4D2/js/index.js
+++ S2W4D2/js/index.js
@@ -4,13 +4,25 @@ const funNav = document.querySelectorAll('nav a');
 
 let col = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() + 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 console.log(col)
+
+function randomColors() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
  
+
+const colorText = document.querySelectorAll('p');
+console.log(colorText)
+colorText.forEach(x => {
+  x.addEventListener('mouseover', (event, i) => {
+    event[i].target.style.color = randomColors();
+    })
+})
 
 // console.log(funNav)
 
 funNav.forEach((x, i) => {
   x.addEventListener('mouseover',() => {
-    funNav[i].style.color = ( col || col);
+    funNav[i].style.color = randomColors();
   })
   x.addEventListener('mouseleave', () => {
     funNav[i].style.color = 'black';
@@ -29,16 +41,17 @@ funTitle.addEventListener('mouseout', () => {
   funTitle.style.visibility = 'visible';
 })
 
-
+// messing with body color and a copy listener
 const source = document.querySelector('body');
-source.addEventListener('copy', (e) => {
+source.addEventListener('copy', () => {
   source.style.backgroundColor = col;
+  
 })
 source.addEventListener('dblclick', () => {
   source.style.backgroundColor = 'white';
 })
 
-
+// using a text selector in text field listener
 function logSelection(event) {
   const log = document.getElementById('log');
   const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
